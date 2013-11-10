7353ea966df3c12c379803ab4eb8f0eecd2134b8
Michael Gaare <michael@clickscape.com>
2013-11-10 12:11:57 -0500

    Some test code in dev/user

--- dev/user.clj
+++ dev/user.clj
@@ -10,8 +10,10 @@
             [lamina.connections :refer (server)]
             [aleph.tcp :as tcp]
             [sauerworld.auth-server.core :as core]
-            [immutant.messaging :as msg]
-            ))
+            [clojure.tools.logging :as log]
+            [immutant.messaging :as msg])
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader)))
 
 (def mykey "")
 
@@ -19,4 +21,46 @@
 
 (def c (l/channel))
 
-(def client (atom {}))
+(def local ["localhost" 28787])
+
+(defn conn-handler [conn]
+  (while (nil? (:exit @conn))
+    (let [msg (.readLine (:in @conn))]
+      (log/info msg))))
+
+(defn connect [[server port]]
+  (let [socket (Socket. server port)
+        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+        out (PrintWriter. (.getOutputStream socket))
+        conn {:in in :out out :socket socket}]
+    conn))
+
+(defn disconnect
+  [conn]
+  (swap! conn assoc :exit true)
+  (let [{:keys [in out socket]} @conn]
+    (.close socket)
+    (.close in)
+    (.close out)
+    conn))
+
+(defn write [conn msg]
+  (let [out (:out @conn)]
+    (.print out (str msg))
+    (.flush out)))
+
+(defn add-handler
+  [conn]
+  (doto (Thread. #(conn-handler conn)) (.start)))
+
+(defonce connection (atom {}))
+
+(defn local-connect
+  []
+  (let [conn (connect local)]
+    (reset! connection conn)
+    (add-handler connection)))
+
+(defn test-write
+  []
+  (write connection "reqauth 20 mefisto\r\n"))
